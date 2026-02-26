169f23cef0b83b025601ffdada428b1c9641c249
msVill <msv.ltmc@gmail.com>
2026-02-26 15:35:04 -0800

    Attempting Lab 14 Start again.

--- 210-lab-14.cpp
+++ 210-lab-14.cpp
@@ -4,26 +4,9 @@
 // Populate them with data and output that data.
 // Comsc 210 | Lab 14 | Martha Stephanie Villalta.
 
+
 #include <iostream>
 using namespace std;
-
-//Color class: Default - private
-class Color {
-    int red, green, blue;
-
-    // public member functions (getters and setters)
-    public:
-    int getRed() { return red; }
-    int getGreen() { return green; }
-    int getBlue() { return blue; }
-
-    void setRed(int r) { red = r; }
-    void setGreen(int g) { green = g; }
-    void setBlue(int b) { blue = b; }
-
-    void print() {
-        cout << "RGB(" << red << ", " << green << ", " << blue << ")" << endl;
-};
 
 int main() {
 
